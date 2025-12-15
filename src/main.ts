4daf0766bd03e3756522dc087e56d70a8ccd35f2
t <a@a.a>
2025-12-15 17:35:19 +0800

    feat: update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const bonjour = require('bonjour')();
+bonjour.publish({
+  name: 'iFCU Control',
+  type: 'http',
+  port: 80,
+  host: 'ifcu-web.local',
+});
+bonjour.find({ type: 'http' }, function (service) {
+  console.log('Found an HTTP server:', service)
+})
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
